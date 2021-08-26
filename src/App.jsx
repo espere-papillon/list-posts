4ea37de59f6e0e8b76a209322b83baf5051aa560
espere-papillon <marinabov007@gmail.com>
2021-08-26 22:51:26 +0300

    update component App and Post

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,38 @@ import React, {useState} from 'react';
 import "./styles/App.css"
 import {PostsList} from "./components/PostsList";
 import {PostForm} from "./components/PostForm";
+import {Select} from "./components/UI/select/Select";
 
 function App() {
   const [posts, setPosts] = useState([
     {id: 1, title: "JS", body: "JS - programming language"},
     {id: 2, title: "Python", body: "Python - programming language"},
     {id: 3, title: "Java", body: "Java - programming language"},
-    ])
+  ])
+  const [selectedSort, setSelectedSort] = useState("")
 
-  const createPost = (post) =>{
+  const createPost = (post) => {
     setPosts([...posts, post])
   }
 
-  const removePost = (post) =>{
+  const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPost = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
     <div className={"App"}>
-      <PostForm create={createPost} />
-      <PostsList posts={posts} title={"Posts list"} remove={removePost} />
+      <PostForm create={createPost}/>
+      <hr className="separator"/>
+      <Select value={selectedSort} onChange={sortPost} defaultValue={"Sorting"}
+              options={[{value: "title", name: "Sort by title"}, {value: "body", name: "Sort by description"}]}/>
+      {posts.length
+        ? <PostsList posts={posts} title={"Posts list"} remove={removePost}/>
+        : <h2 className="labelPostsNotFound">Posts not found</h2>}
     </div>
   );
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,7 +5,7 @@ import {Post} from "./Post";
 export const PostsList = props => {
   return (
     <div >
-      <h1 className={s.postsListTitle}>{props.title}</h1>
+      <h2 className={s.postsListTitle}>{props.title}</h2>
       {props.posts.map((post, index) => <Post key={post.id} number={index+1} post={post} remove={props.remove}/>)}
     </div>
   );
