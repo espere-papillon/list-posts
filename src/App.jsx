3acdc69cf02f3948ae9b0b858d29889fd27d77e7
espere-papillon <marinabov007@gmail.com>
2021-08-26 20:29:38 +0300

    update component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {useState} from 'react';
+import "./styles/App.css"
+import {PostsList} from "./components/PostsList";
+import {PostForm} from "./components/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: "JS", body: "JS - programming language"},
+    {id: 2, title: "Python", body: "Python - programming language"},
+    {id: 3, title: "Java", body: "Java - programming language"},
+    ])
+
+  const createPost = (post) =>{
+    setPosts([...posts, post])
+  }
+
+  const removePost = (post) =>{
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
-    <Counter />
+    <div className={"App"}>
+      <PostForm create={createPost} />
+      <PostsList posts={posts} title={"Posts list"} remove={removePost} />
+    </div>
   );
 }
 
