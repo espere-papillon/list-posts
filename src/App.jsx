e2227a1d68359f3c42883c9372c1adc1cad33fd9
espere-papillon <marinabov007@gmail.com>
2021-08-27 01:06:05 +0300

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React, {useState} from 'react';
 import "./styles/App.css"
 import {PostsList} from "./components/PostsList";
 import {PostForm} from "./components/PostForm";
-import {Select} from "./components/UI/select/Select";
+import {PostFilter} from "./components/PostFilter";
+import {ModalWindow} from "./components/UI/modal/ModalWindow";
+import {Button} from "./components/UI/button/Button";
+import {usePosts} from "./hooks/usePosts"
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,30 +13,29 @@ function App() {
     {id: 2, title: "Python", body: "Python - programming language"},
     {id: 3, title: "Java", body: "Java - programming language"},
   ])
-  const [selectedSort, setSelectedSort] = useState("")
+  const [filter, setFilter] = useState({sort: "", query: ""})
+  const [modal, setModal] = useState(false)
+
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (post) => {
     setPosts([...posts, post])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPost = (sort) => {
-    setSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
-
   return (
     <div className={"App"}>
-      <PostForm create={createPost}/>
+      <Button style={{"marginTop": "20px"}} onClick={() => setModal(true)}>Add post</Button>
+      <ModalWindow visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </ModalWindow>
       <hr className="separator"/>
-      <Select value={selectedSort} onChange={sortPost} defaultValue={"Sorting"}
-              options={[{value: "title", name: "Sort by title"}, {value: "body", name: "Sort by description"}]}/>
-      {posts.length
-        ? <PostsList posts={posts} title={"Posts list"} remove={removePost}/>
-        : <h2 className="labelPostsNotFound">Posts not found</h2>}
+      <PostFilter filter={filter} setFilter={setFilter}/>
+      <PostsList posts={sortedAndSearchedPosts} title={"Posts list"} remove={removePost}/>
     </div>
   );
 }
