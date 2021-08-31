26c9d74dafe69bd327a6ded1aac7760dd90f261c
espere-papillon <marinabov007@gmail.com>
2021-09-01 00:14:28 +0300

    update post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Button} from "../components/UI/button/Button";
+import {useHistory} from 'react-router-dom'
 
 export const Post = props => {
+  const router = useHistory()
   return (
     <div className="post">
       <div>
         <div><strong>{props.post.id}. {props.post.title}</strong></div>
         <div>{props.post.body}</div>
       </div>
-      <div>
+      <div className="post__btn">
+        <Button onClick={() => router.push(`/posts/${props.post.id}`)}>Open</Button>
         <Button onClick={() => props.remove(props.post)}>Delete</Button>
       </div>
     </div>
