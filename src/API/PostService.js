8127a0460d628570effa740d463c10030d05f61f
espere-papillon <marinabov007@gmail.com>
2021-09-01 00:14:01 +0300

    add requests for post and comments

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -10,4 +10,14 @@ export class PostService {
     })
     return response
   }
+
+  static async getById(id) {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    return response
+  }
+
+  static async getComments(id) {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    return response
+  }
 }
