5c031045e49a6fcfd822d9bd1a11bc47c9247c57
espere-papillon <marinabov007@gmail.com>
2021-09-02 01:28:16 +0300

    update posts component

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {PostsList} from "../components/PostsList";
 import {PostForm} from "../components/PostForm";
 import {PostFilter} from "../components/PostFilter";
 import {ModalWindow} from "../components/UI/modal/ModalWindow";
 import {Button} from "../components/UI/button/Button";
+import {Select} from "../components/UI/select/Select";
 import {usePosts} from "../hooks/usePosts"
+import {useObserver} from "../hooks/useObserver"
 import {PostService} from "../API/PostService"
 import {Loader} from "../components/UI/loader/Loader"
 import {useFetching} from "../hooks/useFetching"
@@ -18,18 +20,23 @@ export const Posts = () => {
   const [totalPages, setTotalPages] = useState(0)
   const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
+  const lastElement = useRef()
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostsLoading, postsError] = useFetching(async () => {
+  const [fetchPosts, isPostsLoading, postsError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page)
-    setPosts(response.data)
+    setPosts([...posts, ...response.data])
     const totalCount = response.headers['x-total-count']
     setTotalPages(getPageCount(totalCount, limit))
   })
 
+  useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1)
+  })
+
   useEffect(() => {
-    fetchPosts()
-  }, [page])
+    fetchPosts(limit, page)
+  }, [page, limit])
 
   const createPost = (post) => {
     setPosts([...posts, post])
@@ -52,10 +59,17 @@ export const Posts = () => {
       </ModalWindow>
       <hr className="separator"/>
       <PostFilter filter={filter} setFilter={setFilter}/>
+      <Select value={limit} onChange={value => setLimit(value)} defaultValue="Count of elements on page"
+      options={[
+        {value: 5, name: '5'},
+        {value: 10, name: '10'},
+        {value: 25, name: '25'},
+        {value: -1, name: 'All posts'},
+      ]}/>
       {postsError && <h2 className="labelPostsNotFound">Error: ${postsError}</h2>}
-      {isPostsLoading
-        ? <div className="containerLoader"><Loader/></div>
-        : <PostsList posts={sortedAndSearchedPosts} title={"Posts list"} remove={removePost}/>}
+      <PostsList posts={sortedAndSearchedPosts} title={"Posts list"} remove={removePost}/>
+      <div ref={lastElement} style={{height: 20, backgroundColor: "red"}}/>
+      {isPostsLoading && <div className="containerLoader"><Loader/></div>}
       <Pagination page={page} totalPages={totalPages} changePage={changePage} />
     </div>
   );
