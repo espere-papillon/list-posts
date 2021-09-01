fe6dabfb8e8bcc773cb3a21e3d3abe020aef274a
espere-papillon <marinabov007@gmail.com>
2021-09-02 01:29:37 +0300

    update routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom'
 import {privateRoutes, publicRoutes} from "../router/routes"
+import {AuthContext} from '../context'
+import {Loader} from "./UI/loader/Loader"
 
 export const AppRouter = () => {
-  const isAuth = false
+  const {isAuth, isLoading} = useContext(AuthContext)
+
+  if (isLoading) {
+    return <Loader/>
+  }
+
   return (
     isAuth
     ? <Switch>
-        {privateRoutes.map(route => <Route component={route.component} path={route.path} exact={route.exact}/>)}
-        <Redirect to={"/post"}/>
+        {privateRoutes.map(route => <Route key={route.path} component={route.component} path={route.path} exact={route.exact}/>)}
+        <Redirect to={"/posts"}/>
       </Switch>
     : <Switch>
-        {publicRoutes.map(route => <Route component={route.component} path={route.path} exact={route.exact}/>)}
+        {publicRoutes.map(route => <Route key={route.path} component={route.component} path={route.path} exact={route.exact}/>)}
         <Redirect to={"/login"}/>
       </Switch>
   );
