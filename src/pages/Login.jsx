4888b0faa3e85732cc11ed38ebaddb21b52fa42b
espere-papillon <marinabov007@gmail.com>
2021-09-02 01:26:03 +0300

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Input} from "../components/UI/input/Input";
 import {Button} from "../components/UI/button/Button";
+import {AuthContext} from '../context'
 
 export const Login = () => {
+  const {isAuth, setIsAuth} = useContext(AuthContext)
+
+  const login = event => {
+    event.preventDefault()
+    setIsAuth(true)
+    localStorage.setItem('auth', 'true')
+  }
   return (
     <div>
       <h2 className="containerLoader">Log in</h2>
-      <form>
+      <form onSubmit={login}>
         <Input type="text" placeholder="Login"/>
         <Input type="text" placeholder="Password"/>
         <Button>Log in</Button>
