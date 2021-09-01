16bc1b5f6c74f9a2a6919759668ec26d6cef78f3
espere-papillon <marinabov007@gmail.com>
2021-09-02 01:26:53 +0300

    add log out

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from 'react-router-dom'
+import {Button} from "../button/Button";
+import {AuthContext} from '../../../context'
 
 export const Navbar = () => {
+  const {isAuth, setIsAuth} = useContext(AuthContext)
+
+  const logout = () => {
+    setIsAuth(false)
+    localStorage.removeItem('auth')
+  }
   return (
     <div className={"navbar"}>
+      <Button onClick={logout}>Log out</Button>
       <div className="navbar__links">
         <Link to={"/about"}>About</Link>
         <Link to={"/posts"}>Posts</Link>
